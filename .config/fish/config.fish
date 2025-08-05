a78b9c426c83528366a29a66dbfdcb73b88c6d20
crystalforceix <crystalforceix@gmail.com>
2025-08-05 22:55:53 +0700

    f

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 fish_config theme choose "color"
-oh-my-posh init fish --config $HOME/crystal-dots/.config/terminal-posh-themes/themes1.omp.json | source
+oh-my-posh init fish --config $HOME/i3-dotfiles-template/.config/terminal-posh-themes/themes1.omp.json | source
 if status is-interactive
     set fish_greeting
 end
